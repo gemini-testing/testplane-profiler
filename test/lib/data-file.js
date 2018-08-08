f49be405300c44e02d6475f3ff36270953e35d01
Anton Usmansky <cody0@yandex-team.ru>
2018-08-08 13:09:24 +0300

    WIP

--- test/lib/data-file.js
+++ test/lib/data-file.js
@@ -5,104 +5,220 @@ const DataFile = require('../../lib/data-file');
 
 describe('stream writer', () => {
     const sandbox = sinon.sandbox.create();
-    let streamStub;
+    // let streamStub;
 
     const dataStub = (opts = {}) => {
         return Object.assign(
             {browserId: 'default-browser'},
             opts,
-            {fullTitle: () => opts.fullTitle || 'fullTitle'}
+            {fullTitle: () => opts.fullTitle || 'defaultFullTitle'}
         );
     };
 
+    const mkDataFile = (reportPath = 'default/path') => {
+        return DataFile.create(reportPath);
+    };
+
     beforeEach(() => {
-        streamStub = {
-            write: sinon.stub().named('write'),
-            end: sinon.stub().named('end')
-        };
-        sandbox.stub(fs, 'ensureDirSync');
-        sandbox.stub(fs, 'createWriteStream').returns(streamStub);
+    //     streamStub = {
+    //         write: sinon.stub().named('write'),
+    //         end: sinon.stub().named('end')
+    //     };
+    //     sandbox.stub(fs, 'ensureDirSync');
+    //     sandbox.stub(fs, 'createWriteStream').returns(streamStub);
+
+        sandbox.stub(fs, 'appendFile');
+        sandbox.stub(fs, 'ensureDir').resolves();
+        sandbox.stub(fs, 'open').resolves(12345);
+        sandbox.stub(fs, 'close').resolves();
     });
 
     afterEach(() => sandbox.restore());
 
-    it('should not do any fs stuff on create');
     it('should return promise on end call');
+    it('should not do any fs stuff on create');
 
     describe('create', () => {
-        it('should create directory for report', () => {
-            DataFile.create('report/path');
+        it('should create ensure target dir is exists', async () => {
+            const dataFile = mkDataFile('report/path');
 
-            assert.calledOnceWith(fs.ensureDirSync, 'report/path');
+            await dataFile.end();
+
+            assert.calledOnceWith(fs.ensureDir, 'report/path');
         });
 
-        it('should create write stream to the passed path', () => {
-            DataFile.create('report/path');
+        it('should open data file in target dir', async () => {
+            const dataFile = mkDataFile('report/path');
 
-            assert.calledOnceWith(fs.createWriteStream, 'report/path/data.js');
+            await dataFile.end();
+
+            assert.calledOnceWith(fs.open, 'report/path/data.js');
+        });
+
+        it('should write opening bracket into data file', async () => {
+            fs.open.resolves(100500);
+            const dataFile = mkDataFile();
+
+            await dataFile.end();
+
+            assert.calledWith(fs.appendFile, 100500, 'const data = [');
         });
     });
 
     describe('write', () => {
-        it('should write opening bracket at first call', () => {
-            const stream = DataFile.create('report/path');
+        const getWrittenData = () => {
+            return JSON.parse(fs.appendFile.secondCall.args[1].replace(/,$/, ''));
+        };
 
-            stream.write(dataStub());
+        const write = async (data) => {
+            const dataFile = mkDataFile();
+            dataFile.write(data);
+            await dataFile.end();
+        };
 
-            assert.calledWith(streamStub.write.firstCall, 'const data = [');
+        it('should append data to file', async () => {
+            fs.open.resolves(100500);
+            const dataFile = mkDataFile();
+
+            dataFile.write(dataStub());
+            await dataFile.end();
+
+            assert.calledThrice(fs.appendFile);
+            assert.alwaysCalledWith(fs.appendFile, 100500, sinon.match.string);
         });
 
-        it('should write object with "n" property as "fullTitle"', () => {
-            const stream = DataFile.create('report/path');
-            const data = dataStub({fullTitle: 'test1'});
+        it('should write data with coma at the end', async () => {
+            await write(dataStub());
 
-            stream.write(data);
-
-            assert.propertyVal(JSON.parse(streamStub.write.secondCall.args[0]), 'n', 'test1');
+            const writtenData = fs.appendFile.secondCall.args[1];
+            assert.match(writtenData, /,$/);
         });
 
-        it('should write object with command list from data', () => {
-            const stream = DataFile.create('report/path');
-            const data = dataStub({
-                fullTitle: 'test1',
+        it('should write test full title', async () => {
+            await write(dataStub({
+                fullTitle: 'test1'
+            }));
+
+            assert.match(getWrittenData(), {
+                n: 'test1'
+            });
+        });
+
+        it('should write command list from hermioneCtx', async () => {
+            await write(dataStub({
                 hermioneCtx: {
                     commandList: [{cl: [1]}]
                 }
+            }));
+
+            assert.match(getWrittenData(), {
+                cl: [1]
             });
-
-            stream.write(data);
-            const passedData = JSON.parse(streamStub.write.secondCall.args[0]);
-
-            assert.deepEqual(passedData.cl, [1]);
         });
 
-        it('should write object with empty command list if it does not exist in data', () => {
-            const stream = DataFile.create('report/path');
-            const data = dataStub({fullTitle: 'test1'});
+        it('should write empty command list if there is no command list in data')
 
-            stream.write(data);
-            const passedData = JSON.parse(streamStub.write.secondCall.args[0]);
+        it('should write timings', async () => {
+            await write(dataStub({
+                timeStart: 100400,
+                timeEnd: 100500
+            }));
 
-            assert.deepEqual(passedData.cl, []);
+            assert.match(getWrittenData(), {
+                ts: 100400,
+                te: 100500,
+                d: 100 // duration
+            });
         });
 
-        it('should divide data chains with comma delimiter', () => {
-            const stream = DataFile.create('report/path');
+        it('should write browser data', async () => {
+            await write(dataStub({
+                browserId: 'bro',
+                sessionId: '100500'
+            }));
 
-            stream.write(dataStub());
-            stream.write(dataStub());
-
-            // calls: 1 - open bracket, 2 - first data, 3 - delim, 4 - second data
-            assert.calledWithExactly(streamStub.write.thirdCall, ',');
+            assert.match(getWrittenData(), {
+                bid: 'bro',
+                sid: '100500'
+            });
         });
+
+        it('should write retry', async () => {
+            await write(dataStub({
+                retry: 100500
+            }));
+
+            assert.match(getWrittenData(), {
+                r: 100500
+            });
+        });
+
+        it('should not write retry there is no retry in data', async () => {
+            await write(dataStub());
+
+            assert.notProperty(getWrittenData(), 'r');
+        });
+
+    //     it('should write object with command list from data', () => {
+    //         const stream = DataFile.create('report/path');
+    //         const data = dataStub({
+    //             fullTitle: 'test1',
+    //             hermioneCtx: {
+    //                 commandList: [{cl: [1]}]
+    //             }
+    //         });
+
+    //         stream.write(data);
+    //         const passedData = JSON.parse(streamStub.write.secondCall.args[0]);
+
+    //         assert.deepEqual(passedData.cl, [1]);
+    //     });
+
+    //     it('should write object with empty command list if it does not exist in data', () => {
+    //         const stream = DataFile.create('report/path');
+    //         const data = dataStub({fullTitle: 'test1'});
+
+    //         stream.write(data);
+    //         const passedData = JSON.parse(streamStub.write.secondCall.args[0]);
+
+    //         assert.deepEqual(passedData.cl, []);
+    //     });
+
+    //     it('should divide data chains with comma delimiter', () => {
+    //         const stream = DataFile.create('report/path');
+
+    //         stream.write(dataStub());
+    //         stream.write(dataStub());
+
+    //         // calls: 1 - open bracket, 2 - first data, 3 - delim, 4 - second data
+    //         assert.calledWithExactly(streamStub.write.thirdCall, ',');
+    //     });
     });
 
     describe('end', () => {
-        it('should end stream with the closing bracket', () => {
-            const stream = DataFile.create('report/path');
+        it('should reject if failed to open file', async () => {
+            fs.open.rejects(new Error('foo'));
+            const dataFile = mkDataFile();
 
-            stream.end();
-            assert.calledOnceWith(streamStub.end, ']');
+            await assert.isRejected(dataFile.end(), /foo/);
+        });
+
+        it('should add closing bracket to data file', async () => {
+            fs.open.resolves(100500);
+            const dataFile = mkDataFile();
+
+            await dataFile.end();
+
+            assert.calledWith(fs.appendFile, 100500, ']');
+        });
+
+        it('should close file', async () => {
+            fs.open.resolves(100500);
+            const dataFile = mkDataFile();
+
+            await dataFile.end();
+
+            assert.calledOnceWith(fs.close, 100500);
         });
     });
 });
